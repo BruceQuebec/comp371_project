17a4db5e822e15536555d27843af770a35b6a459
BruceQuebec <oscar_quebec@hotmail.com>
2020-07-21 10:15:31 -0400

    clean code

--- Test/source/load.hpp
+++ Test/source/load.hpp
@@ -1,13 +1,11 @@
 #pragma once
 
-
-#include <vector>
-
 #include <GL/glew.h>
-
+#include <vector>
 
 // Load a shader from a file
 void loadShader(const GLuint & program, GLenum type, const char * file_path);
 
 // Load a model from a file
 void loadModel(std::vector<GLdouble> & pos_data, std::vector<GLdouble> & color_data, std::vector<GLuint> & index_data, GLenum mode, const char * file_path);
+void loadModel(std::vector<GLdouble> & pos_data, std::vector<GLdouble> & color_data, std::vector<GLuint> & index_data, std::vector<GLdouble> & normal_data, std::vector<GLdouble> & texCoords_data, GLenum mode, const char * file_path);
