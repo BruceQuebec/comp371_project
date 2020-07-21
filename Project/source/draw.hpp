872a5ed651b94f8dab1c410e9b80944277b1241d
BruceQuebec <oscar_quebec@hotmail.com>
2020-07-21 09:34:32 -0400

    delcare new methods for implementation of light effect

--- Project/source/draw.hpp
+++ Project/source/draw.hpp
@@ -1,16 +1,16 @@
 #pragma once
 
-
-#include <vector>
-
+#include <unordered_map>
 #include <GL/glew.h>
 #include <glm/gtc/matrix_transform.hpp>
-
 #include "Model.hpp"
+#include "Camera.hpp"
 
 
 // Draw the models
-void draw(glm::mat4 world_mat, std::vector<Model *> models, glm::mat4 camera_mat, GLuint mvp_mat_location);
+void draw(glm::mat4 world_mat, unordered_map<string, Model *> models, glm::mat4 cameraMat, glm::vec3 cameraPos);
 
 // Draw a model
-void draw(glm::mat4 world_mat, Model & model, glm::mat4 camera_mat, GLuint mvp_mat_location);
+void draw(glm::mat4 world_mat, Model & model, glm::mat4 cameraMat, glm::vec3 cameraPos, string model_name);
+
+void lighting_render(Model model, glm::vec3 cameraPos, string model_name);
