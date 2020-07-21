8c4e80d058eaa7c5219e31d631a58b8afe2b4520
BruceQuebec <oscar_quebec@hotmail.com>
2020-07-21 09:41:59 -0400

    clean code

--- Test/source/Model.hpp
+++ Test/source/Model.hpp
@@ -1,29 +1,37 @@
 #pragma once
 
 
-#include <vector>
-
+#include <unordered_map>
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <glm/gtc/matrix_transform.hpp>
+#include "Shader.hpp"
 
 
 class Model
 {
 public:
 	// Vector of models
-	static std::vector<Model *> models;
+	//static std::vector<Model *> models;
+	static unordered_map<string, Model*> models;
 
 private:
 	// Vertex array
 	GLuint vertex_array;
 
+	// the instance of shader being used in the modle
+	Shader shader;
+
 	// Position, color and index data
 	std::vector<GLdouble> pos_data;
 	std::vector<GLdouble> color_data;
 	std::vector<GLuint> index_data;
+	std::vector<GLdouble> normal_data;
+	std::vector<GLdouble> texCoords_data;
 
 	// Number of elements
 	int num_elements;
+	int num_vertices = 0;
 
 	// Mode (line or triangle)
 	GLenum mode;
@@ -42,11 +50,11 @@ private:
 	double z_angle;
 
 public:
-	Model(std::vector<GLdouble> pos_data, std::vector<GLdouble> color_data, std::vector<GLuint> index_data, GLenum mode, double x_pos, double y_pos, double z_pos);
-	Model(GLenum mode, double x_pos, double y_pos, double z_pos, const char * file_path);
+	Model(vector<GLdouble> pos_data, vector<GLdouble> color_data, vector<GLuint> index_data, vector<GLdouble> normal_data, vector<GLdouble> texCoords_data, Shader& shader, unordered_map<string, GLuint> shader_pointer_idx_map, GLenum mode, double x_pos, double y_pos, double z_pos, string model_name);
+	Model(Shader& shader, unordered_map<string, GLuint> shader_pointer_idx_map, GLenum mode, double x_pos, double y_pos, double z_pos, const char * file_path, string model_name);
 
 	// Initialze the model
-	void init(std::vector<GLdouble> pos_data, std::vector<GLdouble> color_data, std::vector<GLuint> index_data, GLenum mode, double x_pos, double y_pos, double z_pos);
+	void init(std::vector<GLdouble> pos_data, std::vector<GLdouble> color_data, std::vector<GLuint> index_data, std::vector<GLdouble> normal_data, std::vector<GLdouble> texCoords_data, Shader shader, unordered_map<string, GLuint> shader_pointer_idx_map, GLenum mode, double x_pos, double y_pos, double z_pos, string model_name);
 
 	// Get the vertex array of the model
 	GLuint getVertexArray();
@@ -54,11 +62,26 @@ public:
 	// Get the number of elements of the model
 	int getNumElements();
 
+	// Get the number of Vertices of the model
+	int getNumVertices();
+
 	// Get the mode of the model
 	GLenum getMode();
 
 	// Get the model matrix of the model
 	glm::mat4 getModelMat();
+
+	// Get Shader of the model
+	Shader getShader();
+
+	// Get position of the model
+	glm::vec3 getPosition();
+
+	// Get normal of the model
+	vector<GLdouble> getNormals(vector<GLdouble> pos_data, vector<GLuint> index_data);
+
+	// Set model scale
+	void setScale(double scale);
 
 	// Key callback of the model
 	void key_callback(int key, int action, int mods);
