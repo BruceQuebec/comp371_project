317a4044b3835d07d6a10ec3f54c7adaa5eae2de
BruceQuebec <oscar_quebec@hotmail.com>
2020-07-24 08:29:48 -0400

    modified file for adding texture

--- Test/source/main.cpp
+++ Test/source/main.cpp
@@ -43,7 +43,8 @@ int main()
 	unordered_map<string, GLuint> shader_pointer_idx_map_generic = {
 								{"pos_data_idx", 0},
 								{"normal_data_idx", 1},
-								{"color_data_idx", 2} };
+								{"color_data_idx", 2},
+								{"texCoords_data_idx", 3} };
 	unordered_map<string, GLuint> shader_pointer_idx_map_lightCube = {
 								{"pos_data_idx", 0},
 								{"normal_data_idx", 1},
@@ -56,13 +57,20 @@ int main()
     Model grid = Model(generic_shader, generic_shader.shader_pointer_idx_map, GL_LINES, 0, 0, 0, "resource/grid.txt", "grid");
     Model axes = Model(generic_shader, generic_shader.shader_pointer_idx_map, GL_LINES, 0, 0, 0, "resource/axes.txt", "axes");
     Model N4 = Model(generic_shader, generic_shader.shader_pointer_idx_map, GL_TRIANGLES, 0, 0, 0, "resource/N4.txt", "N4");
-	Model sphere = Model(generic_shader, generic_shader.shader_pointer_idx_map, GL_TRIANGLE_STRIP, 0, 10, 0, "resource/sphere.txt", "sphere");
-	sphere.setScale(6);
+	Model sphere_N4 = Model(generic_shader, generic_shader.shader_pointer_idx_map, GL_TRIANGLE_STRIP, 0, 10, 0, "resource/sphere.txt", "sphere_N4");
+	sphere_N4.setScale(6);
 	Model L8 = Model(generic_shader, generic_shader.shader_pointer_idx_map, GL_TRIANGLES, -40, 0, -40, "resource/L8.txt", "L8");
+	Model sphere_L8 = Model(generic_shader, generic_shader.shader_pointer_idx_map, GL_TRIANGLE_STRIP, -40, 10, -40, "resource/sphere.txt", "sphere_L8");
+	sphere_L8.setScale(3);
 	Model Z7 = Model(generic_shader, generic_shader.shader_pointer_idx_map, GL_TRIANGLES, 40, 0, -40, "resource/Z7.txt", "Z7");
-    Model I4 = Model(generic_shader, generic_shader.shader_pointer_idx_map, GL_TRIANGLES, -40, 0, 40, "resource/I4.txt", "I4");
+	Model sphere_Z7 = Model(generic_shader, generic_shader.shader_pointer_idx_map, GL_TRIANGLE_STRIP, 40, 10, -40, "resource/sphere.txt", "sphere_Z7");
+	sphere_Z7.setScale(3);
+	Model I4 = Model(generic_shader, generic_shader.shader_pointer_idx_map, GL_TRIANGLES, -40, 0, 40, "resource/I4.txt", "I4");
+	Model sphere_I4 = Model(generic_shader, generic_shader.shader_pointer_idx_map, GL_TRIANGLE_STRIP, -40, 10, 40, "resource/sphere.txt", "sphere_I4");
+	sphere_I4.setScale(3);
     Model E7 = Model(generic_shader, generic_shader.shader_pointer_idx_map, GL_TRIANGLES, 40, 0, 40, "resource/E7.txt", "E7");
-	
+	Model sphere_E7 = Model(generic_shader, generic_shader.shader_pointer_idx_map, GL_TRIANGLE_STRIP, 40, 10, 40, "resource/sphere.txt", "sphere_E7");
+	sphere_E7.setScale(3);
 	Model lightCube = Model(lightCube_shader, lightCube_shader.shader_pointer_idx_map, GL_TRIANGLES, 0,30,5, "resource/lightCube.txt", "lightCube");
 
     // Initialize a camera
@@ -83,11 +91,20 @@ int main()
 
     // Set the models to be controlled
 	Control::setModel(&N4, 1);
+	Control::setModel(&sphere_N4, 1);
+
 	Control::setModel(&L8, 2);
+	Control::setModel(&sphere_L8, 2);
+
     Control::setModel(&Z7, 3);
+	Control::setModel(&sphere_Z7, 3);
+
     Control::setModel(&I4, 4);
+	Control::setModel(&sphere_I4, 4);
+
     Control::setModel(&E7, 5);
-	Control::setModel(&sphere, 6);
+	Control::setModel(&sphere_E7, 5);
+	
 
     // Enable the depth test
     glEnable(GL_DEPTH_TEST);
