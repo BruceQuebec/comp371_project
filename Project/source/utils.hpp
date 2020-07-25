43fb603ddea777138e3287c2772cfdab37948d8f
BruceQuebec <oscar_quebec@hotmail.com>
2020-07-25 01:15:40 -0400

    added new method to get normal vector from vertex position data

--- Project/source/utils.hpp
+++ Project/source/utils.hpp
@@ -1,9 +1,11 @@
 #pragma once
 
 #include <vector>
+#include <unordered_map>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtx/string_cast.hpp>
 
 using std::vector;
 
@@ -45,6 +47,41 @@ public:
 			norm_data.push_back(vertex.x);
 			norm_data.push_back(vertex.y);
 			norm_data.push_back(vertex.z);
+		}
+		return norm_data;
+	}
+
+	static vector<GLdouble> getNormalForVertex(vector<GLdouble> pos_data) {
+		std::unordered_map<string, glm::vec3> verticesNormal;
+		std::vector<GLdouble> norm_data;
+
+		for (int i = 0; i < pos_data.size(); i += 9)
+		{
+			// Organize vertex position data from pos_data vectors
+			glm::vec3 curVertex = glm::vec3(pos_data[i], pos_data[i + 1], pos_data[i + 2]);
+			glm::vec3 firstNextVertex = glm::vec3(pos_data[i+3], pos_data[i + 4], pos_data[i + 5]);
+			glm::vec3 secondNextVertex = glm::vec3(pos_data[i + 6], pos_data[i + 7], pos_data[i + 8]);
+			// Get the face normal
+			glm::vec3 vector1 = firstNextVertex - curVertex;
+			glm::vec3 vector2 = secondNextVertex - curVertex;
+			glm::vec3 faceNormal = glm::cross(vector1, vector2);
+
+			// Add the face normal to the 3 vertices normal touching this face
+			verticesNormal[glm::to_string(curVertex)] += faceNormal;
+			verticesNormal[glm::to_string(firstNextVertex)] += faceNormal;
+			verticesNormal[glm::to_string(secondNextVertex)] += faceNormal;
+		}
+
+		// Normalize vertices normal
+		std::unordered_map<string, glm::vec3>::iterator it = verticesNormal.begin();
+		while (it != verticesNormal.end()) {
+			if (!(it->second.x == 0 && it->second.y == 0 && it->second.z == 0)) {
+				it->second = glm::normalize(it->second);
+			}
+			norm_data.push_back(it->second.x);
+			norm_data.push_back(it->second.y);
+			norm_data.push_back(it->second.z);
+			it++;
 		}
 		return norm_data;
 	}
