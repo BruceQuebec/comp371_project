fec31efbd17b4259a58e8797d67934b59bc2ec22
BruceQuebec <oscar_quebec@hotmail.com>
2020-07-27 15:18:58 -0400

    added point light shadow render

--- Test/source/draw.hpp
+++ Test/source/draw.hpp
@@ -8,19 +8,15 @@
 
 
 // Draw the models
-void draw(glm::mat4 world_mat, unordered_map<string, Model *> models, glm::mat4 cameraMat, glm::vec3 cameraPos, unordered_map<string, unsigned int> depthMapIDs, glm::mat4 lightSpaceMatrix);
+void draw(glm::mat4 world_mat, unordered_map<string, Model *> models, glm::mat4 cameraMat, glm::vec3 cameraPos, unordered_map<string, unsigned int> depthMapIDs, float far_plane);
 
 // Draw a model
-void draw(glm::mat4 world_mat, Model & model, glm::mat4 cameraMat, glm::vec3 cameraPos, string model_name, unordered_map<string, unsigned int> depthMapIDs, glm::mat4 lightSpaceMatrix);
+void draw(glm::mat4 world_mat, Model & model, glm::mat4 cameraMat, glm::vec3 cameraPos, string model_name, unordered_map<string, unsigned int> depthMapIDs, float far_plane);
 
-void lighting_render(Model model, glm::vec3 cameraPos, string model_name);
+void lighting_render(Model model, glm::vec3 cameraPos, string model_name, float far_plane);
 
-unordered_map<string, unsigned int> depthMappingSetup(Shader debugDepthQuadShader);
+unordered_map<string, unsigned int> depthMappingSetup();
 
-glm::mat4 depthMappingPerform(Shader debugDepthQuadShader, Shader shadowMappingShader, glm::vec3 lightSourcePos, Model ground, unordered_map<string, unsigned int> depthMapIDs, glm::mat4 world_mat, unordered_map<string, Model *> models);
+float depthMappingPerform(Shader shadowMappingShader, glm::vec3 lightSourcePos, unordered_map<string, unsigned int> depthMapIDs, glm::mat4 world_mat, unordered_map<string, Model *> models);
 
 void renderDepthMapScene(Shader &depth_mapping_shader, glm::mat4 world_mat, unordered_map<string, Model *> models);
-
-void renderQuad();
-
-void renderCube();
