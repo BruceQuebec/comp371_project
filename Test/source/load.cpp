188ad5f300e663219f536e6e1290252b2e4328c8
BruceQuebec <oscar_quebec@hotmail.com>
2020-07-26 23:59:16 -0400

    deleted useless loadmodel method

--- Test/source/load.cpp
+++ Test/source/load.cpp
@@ -31,60 +31,6 @@ void loadShader(const GLuint & program, GLenum type, const char * file_path)
 }
 
 
-void loadModel(vector<GLdouble> & pos_data, vector<GLdouble> & color_data, vector<GLuint> & index_data, GLenum mode, const char * file_path)
-{
-	ifstream model_stream(file_path);
-
-	string line;
-	while (getline(model_stream, line))
-	{
-		if (line.length() != 0)
-		{
-			stringstream string_stream(line);
-
-			double x_data, y_data, z_data;
-			unsigned int index1, index2, index3;
-
-			char label;
-			string_stream >> label;
-			if (label == 'p')
-			{
-				string_stream >> x_data >> y_data >> z_data;
-
-				pos_data.push_back(x_data);
-				pos_data.push_back(y_data);
-				pos_data.push_back(z_data);
-			}
-			else if (label == 'c')
-			{
-				string_stream >> x_data >> y_data >> z_data;
-
-				color_data.push_back(x_data);
-				color_data.push_back(y_data);
-				color_data.push_back(z_data);
-			}
-			else if (label == 'i')
-			{
-				if (mode == GL_LINES)
-				{
-					string_stream >> index1 >> index2;
-
-					index_data.push_back(index1);
-					index_data.push_back(index2);
-				}
-				else if (mode == GL_TRIANGLES)
-				{
-					string_stream >> index1 >> index2 >> index3;
-
-					index_data.push_back(index1);
-					index_data.push_back(index2);
-					index_data.push_back(index3);
-				}
-			}
-		}
-	}
-}
-
 void loadModel(std::vector<GLdouble> & pos_data, std::vector<GLdouble> & color_data, std::vector<GLuint> & index_data, std::vector<GLdouble> & normal_data, std::vector<GLdouble> & texCoords_data, GLenum mode, const char * file_path) {
 	ifstream model_stream(file_path);
 
